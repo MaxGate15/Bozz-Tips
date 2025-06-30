cac22fd00aca6c83c5dce07f21269df340b14918
Kobby24 <geffah005@st.ug.edu.gh>
2025-06-30 22:51:12 +0000

    Comment out Google Sign In button in LoginPage component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -54,7 +54,7 @@ export default function LoginPage() {
         )}
 
         {/* Google Sign In Button */}
-        <button
+        {/* <button
           onClick={handleGoogleSignIn}
           className="w-full flex items-center justify-center gap-3 py-3 px-4 border border-gray-300 rounded-lg text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
         >
@@ -77,7 +77,7 @@ export default function LoginPage() {
             />
           </svg>
           Sign in with Google
-        </button>
+        </button> */}
 
         <div className="relative my-6">
           <div className="absolute inset-0 flex items-center">
